1fcdbe300e90f16c2635294464599254c3dbc390
Giorgio Gamba <giorgiogamba97@gmail.com>
2024-12-27 15:40:12 +0100

    add compile script for shaders

---
+++ lava/lava/compile.sh
@@ -0,0 +1,2 @@
+/Users/giorgiogamba/VulkanSDK/1.3.296.0/macOS/bin/glslc shaders/vertex_shader.vert -o shaders/vertex_shader.vert.spv
+/Users/giorgiogamba/VulkanSDK/1.3.296.0/macOS/bin/glslc shaders/fragment_shader.frag -o shaders/fragment_shader.frag.spv
