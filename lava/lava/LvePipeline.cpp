0b0dc28eb06740b86fa06bcab5b3ffad0fd90fe4
Giorgio Gamba <giorgiogamba97@gmail.com>
2024-12-27 15:40:45 +0100

    add Pipeline definition

--- lava/lava/LvePipeline.cpp
+++ lava/lava/LvePipeline.cpp
@@ -5,4 +5,48 @@
 //  Created by Giorgio Gamba on 27/12/24.
 //
 
+#include <fstream>
+#include <iostream>
+
 #include "LvePipeline.hpp"
+
+namespace Lve {
+
+LvePipeline::LvePipeline(const std::string& vertexShaderPath, const std::string& fragmentShaderPath)
+{
+    createPipeline(vertexShaderPath, fragmentShaderPath);
+}
+
+std::vector<char> LvePipeline::readFile(const std::string& filePath)
+{
+    // ate makes the file stream seek to the end immediately
+    // binary makes the file read as binary (because we already compiled it)
+    std::ifstream filestream(filePath, std::ios::ate | std::ios::binary);
+    
+    if (!filestream.is_open())
+    {
+        throw std::runtime_error("Failed to open file: " + filePath);
+    }
+    
+    size_t size = static_cast<size_t>(filestream.tellg());
+    std::vector<char> buffer(size);
+    
+    filestream.seekg(0);
+    filestream.read(buffer.data(), size);
+    
+    filestream.close();
+    
+    return buffer;
+}
+
+void LvePipeline::createPipeline(const std::string& vertexShaderPath, const std::string& fragmentShaderPath)
+{
+    const std::vector<char>& vertexShader = readFile(vertexShaderPath);
+    const std::vector<char>& fragmentShader = readFile(fragmentShaderPath);
+    
+    std::cout << "Shaders allocation completed" << "\n";
+    std::cout << "Vertex shader size: " << vertexShader.size() << "\n";
+    std::cout << "Fragment shader size: " << fragmentShader.size() << std::endl;
+}
+
+}
--- lava/lava/LvePipeline.hpp
+++ lava/lava/LvePipeline.hpp
@@ -5,9 +5,33 @@
 //  Created by Giorgio Gamba on 27/12/24.
 //
 
+#pragma once
+
 #ifndef LvePipeline_hpp
 #define LvePipeline_hpp
 
 #include <stdio.h>
+#include <string>
+#include <vector>
+
+#include "LvePipeline.hpp"
 
 #endif /* LvePipeline_hpp */
+
+namespace Lve {
+
+class LvePipeline
+{
+public:
+    
+    LvePipeline(const std::string& vertexShaderPath, const std::string& fragmentShaderPath);
+    
+private:
+    
+    static std::vector<char> readFile(const std::string& filePath);
+    
+    void createPipeline(const std::string& vertexShaderPath, const std::string& fragmentShaderPath);
+    
+};
+
+}
