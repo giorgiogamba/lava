a0dacd6dccd83114acf713d48fcbaf26a1afeb95
Giorgio Gamba <giorgiogamba97@gmail.com>
2024-12-27 15:39:12 +0100

    change test triangle definition inside vertex shader

---
+++ lava/lava/LvePipeline.cpp
@@ -0,0 +1,8 @@
+//
+//  LvePipeline.cpp
+//  lava
+//
+//  Created by Giorgio Gamba on 27/12/24.
+//
+
+#include "LvePipeline.hpp"
---
+++ lava/lava/LvePipeline.hpp
@@ -0,0 +1,13 @@
+//
+//  LvePipeline.hpp
+//  lava
+//
+//  Created by Giorgio Gamba on 27/12/24.
+//
+
+#ifndef LvePipeline_hpp
+#define LvePipeline_hpp
+
+#include <stdio.h>
+
+#endif /* LvePipeline_hpp */
