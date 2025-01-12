3926f78b7f5eabd47f3ca8fdb9099b666e2c2334
Giorgio Gamba <giorgiogamba97@gmail.com>
2025-01-12 17:41:13 +0100

    add basic Render System first implementation

---
+++ lava/lava/RenderSystem.cpp
@@ -0,0 +1,8 @@
+//
+//  RenderSystem.cpp
+//  lava
+//
+//  Created by Giorgio Gamba on 10/01/25.
+//
+
+#include "RenderSystem.hpp"
---
+++ lava/lava/RenderSystem.hpp
@@ -0,0 +1,13 @@
+//
+//  RenderSystem.hpp
+//  lava
+//
+//  Created by Giorgio Gamba on 10/01/25.
+//
+
+#ifndef RenderSystem_hpp
+#define RenderSystem_hpp
+
+#include <stdio.h>
+
+#endif /* RenderSystem_hpp */
