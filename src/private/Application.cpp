5ca8c58a94fefbd66cb8f8c41cb20ad98b4c2e32
Giorgio Gamba <giorgiogamba97@gmail.com>
2025-03-19 12:22:41 +0100

    enable fragment shading

--- src/private/Application.cpp
+++ src/private/Application.cpp
@@ -56,7 +56,7 @@ void Application::Run()
     }
 
     auto GlobalSetLayout = LavaDescriptorSetLayout::Builder(Device)
-        .addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT)
+        .addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS)
         .build();
 
     std::vector<VkDescriptorSet> GlobalDescriptorSets(LavaSwapChain::MAX_FRAMES_IN_FLIGHT);
