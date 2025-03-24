a911dfe1b857e236ee07dbf5b0651c0a9f2683c4
Giorgio Gamba <giorgiogamba97@gmail.com>
2025-03-24 15:59:41 +0100

    update compile script for point light shaders

--- src/compile.sh
+++ src/compile.sh
@@ -1,2 +1,4 @@
 /Users/giorgiogamba/VulkanSDK/1.3.296.0/macOS/bin/glslc shaders/vertex_shader.vert -o shaders/vertex_shader.vert.spv
 /Users/giorgiogamba/VulkanSDK/1.3.296.0/macOS/bin/glslc shaders/fragment_shader.frag -o shaders/fragment_shader.frag.spv
+/Users/giorgiogamba/VulkanSDK/1.3.296.0/macOS/bin/glslc shaders/point_light_shader.vert -o shaders/point_light_shader.vert.spv
+/Users/giorgiogamba/VulkanSDK/1.3.296.0/macOS/bin/glslc shaders/point_light_shader.frag -o shaders/point_light_shader.frag.spv
